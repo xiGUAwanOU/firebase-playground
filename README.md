525e26a9a11852fd2da226007c27ef53d4c41e4f
xiGUAwanOU <xiguawanou@gmail.com>
2020-10-22 21:31:25 +0200

    Started to play with React.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# firebase-playground
+# Firebase Playground
+
+This is a playground for Firebase technologies. This will also be a learning project for React.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello world!</h1>
+      <p>This is a playground for firebase.</p>
+    </div>
+  );
+}
+
+export default App;
