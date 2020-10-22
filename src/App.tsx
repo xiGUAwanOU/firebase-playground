aa347a215f1cab9b98bd538b06831b17e7c0c545
xiGUAwanOU <xiguawanou@gmail.com>
2020-10-22 23:29:18 +0200

    Added react-router-dom and added Home page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello world!</h1>
-      <p>This is a playground for firebase.</p>
+      <Router>
+        <Link to="/">Home</Link>
+        <hr />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <h1>Firebase Playground</h1>
+        <p>This is a playground for the services in Firebase.</p>
+        <p>Click the links above to see the details of each service.</p>
+      </div>
+    );
+  }
+}
